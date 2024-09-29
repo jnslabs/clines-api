f07ffeb71c3a9fd0cbf9094ef0046b7d5265005a
Jairo Nascimento <jaironsousa@gmail.com>
2024-09-29 11:58:28 -0300

    feat(infra): configuração conexão banco com flywaye entidade Users

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # clines-api
+## [Initializer](https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.3.4&packaging=jar&jvmVersion=21&groupId=com.jnsdev.clines-api&artifactId=clines-api&name=clines-api&description=Api%20exemplo%20para%20deploy%20na%20aws&packageName=com.jnsdev.clines-api&dependencies=web,lombok,data-jpa,postgresql,configuration-processor,flyway)
+
+## Repositorio
+
+[clines-api](https://github.com/jnslabs/clines-api)
+
+## Tecnologias Utilizadas
+
+* Spring-boot
+* Spring-boot-jpa
+* Postgres
+* Bean Validacion
+* Flayway Migrations
+* Lombok
+* Github Action
+* AWS
---
+++ src/main/java/com/jnsdev/clines_api/repository/Entity/User.java
@@ -0,0 +1,34 @@
+package com.jnsdev.clines_api.repository.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 29/09/2024 - 11:38
+ */
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
