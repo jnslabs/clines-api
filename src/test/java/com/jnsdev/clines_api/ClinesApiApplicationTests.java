6f8e1f8ccca3e37f66b380ef7f4a83955a834023
Jairo Nascimento <jaironsousa@gmail.com>
2024-09-29 09:54:59 -0300

    Projeto inicial

---
+++ src/test/java/com/jnsdev/clines_api/ClinesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnsdev.clines_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
